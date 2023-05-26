54a47a8ab1374a7fc8a2b6970c482749587bd655
TuanNgoc1601 <ngocndt@bsscommerce.com>
2023-05-26 21:57:14 +0700

    delete firebase

--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import userController from '../controllers/userController';
 import courseController from '../controllers/courseController';
-import mediaController from '../controllers/mediaController';
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -11,14 +10,10 @@ let initWebRoutes = (app) => {
     router.post("/api/create-new-user", userController.handleCreateNewUser);
     router.put("/api/edit-user", userController.handleEditUser);
     router.delete("/api/delete-user", userController.handleDeleteUser);
-    router.get("/api/gett-url-image",mediaController.getIUrl);
-    router.get("/api/gett-url-video",mediaController.getVUrl);
+
     router.get("/api/get-all-words", courseController.handleGetAllWords);
     router.get("/api/get-all-courses", courseController.handleGetAllCourses);
     router.get("/api/get-content", courseController.handleVideoofWord);
-    
-    router.post("/api/upload-image", mediaController.uploadI);
-    
     
     return app.use("/", router);
 }
