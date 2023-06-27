2df3bdd664b2c82d4ed7488991eeba77be613196
lqm_mpl <lequocmanhno1@gmail.com>
2023-06-27 16:18:22 +0700

    update_db

---
+++ src/models/users_tests.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users_Tests extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Users_Tests.init({
+    user_id: DataTypes.INTEGER,
+    test_id: DataTypes.INTEGER,
+    is_correct: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Users_Tests',
+  });
+  return Users_Tests;
+};
